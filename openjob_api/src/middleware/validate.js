7dae2354f0d18464523c042c0a9a35b990545782
RezaHarahap <114418673+RezaHarahap@users.noreply.github.com>
2026-09-05 13:31:34 +0700

    feat: tambahkan middleware validasi

---
+++ openjob_api/src/middleware/validate.js
@@ -0,0 +1,8 @@
+module.exports = (schema) => (req, res, next) => {
+  const { error, value } = schema.validate(req.body, { abortEarly: false, stripUnknown: true });
+  if (error) {
+    return res.status(400).json({ status: 'failed', message: error.details.map((d) => d.message).join(', ') });
+  }
+  req.body = value;
+  return next();
+};
